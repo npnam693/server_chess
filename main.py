da7ee24c0d443b798be69ac4faec1d08b659a9ca
Nam Nguyễn <nguyenphinam2k2@gmail.com>
2023-04-09 14:19:16 +0700

    alo server

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 from flask import Flask
-import socket_setup
+from socket_setup import socket_run
 
 app = Flask(__name__)
-
 @app.route('/')
 def run_server():
+    socket_run()
     return 'Server is running'
 
 if __name__ == '__main__':
-    socket_setup.socket_run()   
-    app.run(debug=True)
+    app.run()
